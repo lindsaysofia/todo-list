ff40a8d6fe3af7f2b4cf703220762c6699f9ab29
Lindsay Lopez <lopez95@berkeley.edu>
2021-07-01 17:49:28 -0700

    ran build and fixed index.js

--- src/index.js
+++ src/index.js
@@ -1,9 +1,4 @@
 import './style.css';
-import todo from './todo';
-import project from './project';
-import domLogic from './domLogic';
 import applicationLogic from './applicationLogic';
 
-const todoList = (function(){
-  applicationLogic.initiateTodoProject();
-})();
+applicationLogic.initiateTodoProject();
